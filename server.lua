25bbaf0d79302cffbc513244aba3540e28123111
BeansFL <106496863+BeansFL@users.noreply.github.com>
2022-11-24 05:20:46 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,10 +1,9 @@
-local hook = 'YOUR_WEBHOOK' -- set your webhook here!
+local hook = 'YOUR_WEBHOOK'
 
 RegisterServerEvent('beansaimlogs:log')
 AddEventHandler('beansaimlogs:log', function(pedId)
-    local _source = source 
+    local _source = source
     local name = GetPlayerName(_source)
     local targetName = GetPlayerName(pedId)
-    PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="__**Aim Logs**__",description="\nPlayer: "..name.. "`[".._source.."]`\nIs aiming: "..targetName.." `["..pedId.."]`",color=16711680}}}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(hook, function(err, text, headers) end, 'POST', json.encode({embeds={{title="__**Aim Logs**__",description="\nSpieler: "..name.. "`[".._source.."]`\nZielt auf: "..targetName.." `["..pedId.."]`",color=16711680}}}), { ['Content-Type'] = 'application/json' })
 end)
-  
